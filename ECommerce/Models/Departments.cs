9cd9940abdf4cbddca0af5773180fdd4a228d8e9
Fernando <fmti91@hotmail.com>
2018-03-07 19:26:25 -0300

    07: Adding primary key (DepartmentsId) and required (Name with error message) in Departments.cs.

--- ECommerce/Models/Departments.cs
+++ ECommerce/Models/Departments.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ECommerce.Models
 {
     public class Departments
     {
         //prop 2 times tab (auto)
+        [Key] //primary key
         public int DepartmentsId { get; set; }
+        [Required(ErrorMessage = "Required {0}!")] //required with error message // {0} field name
         public string Name { get; set; }
         //necessary to build the project (Build > Build Solution)
     }
